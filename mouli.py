66bf8e4fd04402387eb6ec8892a11b7a267b5888
moulali39 <107177595+moulali39@users.noreply.github.com>
2022-06-15 14:55:25 +0530

    Add files via upload

---
+++ mouli.py
@@ -0,0 +1,16 @@
+#h='moulali'
+#print(type(h))
+#print("my word is very be'auity")
+#print("my word is very be' \ auity")
+#print("my word iss very''' \ 
+#      \   different")
+print("moiskhd''jsnc")
+a= 35
+b= 45
+c= 55
+print(a)
+# print(b)
+# print(c)
+print(d)   # where the d is not defining so it will de not defined in the system
+
+
